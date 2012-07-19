44908513e3b46ef06299b546d84e80731ee19b2f
ozn <ozn@debian-oz.science-computing.de>
2012-07-19 18:19:10 +0200

    added the file to include all dependencies.

---
+++ install_mate_dependencies.sh
@@ -0,0 +1,12 @@
+aptitude install libxml2-dev libxslt1-dev libglib2.0-dev libidl-dev \
+    libdbus-1-dev libdbus-glib-1-dev libpolkit-backend-1-dev flex libpopt-dev \
+    bison libbz2-dev libgcrypt11-dev libcanberra-dev libgail-dev libgtk2.0-dev\
+    libart-2.0-dev libglade2-dev libtasn1-3-bin libxklavier-dev libsoup2.4-dev\
+    icon-naming-utils libunique-dev libcanberra-gtk-dev libwnck-dev \
+    librsvg2-dev libpolkit-agent-1-dev gobject-introspection \
+    libgirepository1.0-dev libupower-glib-dev intltool xsltproc \
+    libtasn1-3-dev libtool gtk-doc-tools libgamin-dev \
+    librarian-dev  libpam0g-dev dh-autoreconf  python-all-dev \ 
+    python-gobject-dev python-gtk2-dev librsvg2-bin  libexempi-dev \ 
+    gir1.2-gtk-2.0  libmatewnck-dev libgconf2-deva  libgtop2-dev \ 
+    libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libpulse-dev
